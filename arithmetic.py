edb9624e4a7e898d9901ccffe13ac63d2d7b8a25
saamyukhtha21 <samyukhthakumaran@karunya.edu.in>
2025-02-14 12:03:46 +0530

    Update arithmetic.py

--- arithmetic.py
+++ arithmetic.py
@@ -1,9 +1,11 @@
 def add(a,b):
   return a+b
 def sub(c,d):
-  return c*d
+  return c-d
 def mul(e,f):
-  return e-f
-add(3+5)
-sub(8-7)
-mul(7*6)
+  return e*f
+print(add(3,5))
+print(sub(8,7))
+print(mul(7,6))
+
+
